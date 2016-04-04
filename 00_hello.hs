fd6951d7d0cc235dbfabfd5612fb7043ed9b28f9
ao1 <ao1@ra.rockwell.com>
2016-04-04 13:42:59 +0800

    setup haskell window environment. Write hello programs

---
+++ 00_hello.hs
@@ -0,0 +1,6 @@
+-- c:\minghc-7.10.2-x86_64\start_haskell.bat
+main = do
+  print "Hello!"
+  name <- getLine
+  print ("Hello " ++ name ++ "!") 
+  
---
+++ hello.hs
@@ -0,0 +1,3 @@
+-- c:\minghc-7.10.2-x86_64\start_haskell.bat
+main = putStrLn "Hello!"
+  
