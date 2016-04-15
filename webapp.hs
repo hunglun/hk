4df6c5923d923b11f3ca767f6fbbdc7e441b596a
hunglun <hunglun.o@gmail.com>
2016-04-16 04:10:59 +0800

    Yesod web server
    It is easy to install.

--- webapp.hs
+++ webapp.hs
@@ -1,4 +1,30 @@
-import System.Environment
-main = do
-  print "This is Hung Lun's haskell echo web server!"
-  interact $ unlines . lines
+{-# LANGUAGE TypeFamilies, QuasiQuotes, MultiParamTypeClasses,
+             TemplateHaskell, OverloadedStrings #-}
+import Yesod
+
+data MyWebSite = MyWebSite
+
+instance Yesod MyWebSite
+
+mkYesod "MyWebSite" [parseRoutes|
+  / HomeR GET
+|]
+
+-- show
+getHomeR = defaultLayout $ do
+  [whamlet|
+    <h2> Things To Do
+    <ul>
+      <li> Learn Haskell
+      <li> Write a killer app
+      <li> Create a startup
+      <li> Go public
+  |]
+  toWidget [cassius|
+    body
+      background-color: #edf
+  |]
+-- /show
+-- how to set PORT in environment?
+-- main = warpEnv MyWebSite
+main = warpDebug 3000 MyWebSite 
