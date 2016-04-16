1ccd8e506b92264a4f2dc9839b540028c0b4a7f5
hunglun <hunglun.o@gmail.com>
2016-04-16 15:41:09 +0800

    renderHtmlBuilder

--- webapp.hs
+++ webapp.hs
@@ -1,17 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
-import           Blaze.ByteString.Builder (Builder, fromByteString)
-import           Network.HTTP.Types       (status200)
-import           Network.Wai              (Application, responseBuilder)
-import           Network.Wai.Handler.Warp (run)
+import           Network.HTTP.Types            (status200)
+import           Network.Wai                   (Application, responseBuilder)
+import           Network.Wai.Handler.Warp      (run)
+import           Text.Blaze.Html.Renderer.Utf8 (renderHtmlBuilder)
+import           Text.Blaze.Html5              (Html, docTypeHtml)
+import qualified Text.Blaze.Html5              as H
 
--- http://www.yesodweb.com/book/yesod-for-haskellers
--- Building Html Response    
 main :: IO ()
 main = run 3000 app
-app :: Application
 
+app :: Application
 app _req sendResponse = sendResponse $ responseBuilder
     status200
-    [("Content-Type", "text/plain")]
-    (fromByteString "Hello from blaze-builder!" :: Builder)
+    [("Content-Type", "text/html")] 
+    (renderHtmlBuilder myPage)
 
+myPage :: Html
+myPage = docTypeHtml $ do
+    H.head $ do
+        H.title "Hello from blaze-html and Warp"
+    H.body $ do
+        H.h1 "Hello from blaze-html and Warp"
