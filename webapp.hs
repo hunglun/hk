5a9bce49fce5801c14d0ee78435fe7a6f21df015
hunglun <hunglun.o@gmail.com>
2016-04-16 16:09:53 +0800

    streaming
    safari display data, after web server flush and terminates. I expect
    that safari immediately displays data, after flush.

--- webapp.hs
+++ webapp.hs
@@ -1,23 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
-import           Network.HTTP.Types            (status200)
-import           Network.Wai                   (Application, responseBuilder)
-import           Network.Wai.Handler.Warp      (run)
-import           Text.Blaze.Html.Renderer.Utf8 (renderHtmlBuilder)
-import           Text.Blaze.Html5              (Html, docTypeHtml)
-import qualified Text.Blaze.Html5              as H
+import           Blaze.ByteString.Builder           (Builder, fromByteString)
+import           Blaze.ByteString.Builder.Char.Utf8 (fromShow)
+import           Control.Concurrent                 (threadDelay)
+import           Control.Monad                      (forM_)
+import           Control.Monad.Trans.Class          (lift)
+import           Data.Monoid                        ((<>))
+import           Network.HTTP.Types                 (status200)
+import           Network.Wai                        (Application,
+                                                     responseStream,StreamingBody)
+import           Network.Wai.Handler.Warp           (run)
 
 main :: IO ()
 main = run 3000 app
 
 app :: Application
-app _req sendResponse = sendResponse $ responseBuilder
+app _req sendResponse = sendResponse $ responseStream
     status200
-    [("Content-Type", "text/html")] 
-    (renderHtmlBuilder myPage)
+    [("Content-Type", "text/plain")]
+    myStream
 
-myPage :: Html
-myPage = docTypeHtml $ do
-    H.head $ do
-        H.title "Hello from blaze-html and Warp"
-    H.body $ do
-        H.h1 "Hello from blaze-html and Warp"
+myStream :: StreamingBody
+myStream send flush = do
+    send $ fromByteString "Starting streaming response.\n"
+    send $ fromByteString "Performing some I/O.\n"
+    flush
+    
+    send $ fromByteString "I/O performed, here are some results.\n"
+    forM_ [1..50 :: Int] $ \i -> do
+        send $ fromByteString "Got the value: " <>
+               fromShow i <>
+               fromByteString "\n"
+        threadDelay 1000000
