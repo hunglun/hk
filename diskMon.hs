2618311a3695fced31aa572c8be83c089eba3f95
ao1 <ao1@ra.rockwell.com>
2016-04-19 17:49:51 +0800

    disk free capacity monitor tool

---
+++ diskMon.hs
@@ -0,0 +1,19 @@
+import System.Process
+freeByte :: String -> Integer
+freeByte  = read . dropWhile (\x -> notElem x ['0'..'9']) 
+
+-- if free bytes fall below 50 GB, return FALSE
+storageCapacityTooLow :: Integer -> Bool
+storageCapacityTooLow n = n < 50 * 1000 * 1000 * 1000
+
+-- usage
+-- c:\Users\ao1\code\hk>fsutil volume diskfree b: | runghc diskMon.hs
+main = do
+  -- first line describes the free bytes
+  -- c:/Users/ao1/bcserver>fsutil volume diskfree b:
+  -- Total # of free bytes        : 202043080704
+  line <- getLine
+  print $ freeByte line
+  if (storageCapacityTooLow . freeByte) line
+  then createProcess (shell "c:/Python27/Lib/site-packages/rac/ramail.py ao1 \"B Drive free storage capacity too low! \"" )
+  else createProcess (shell "rem" )
--- start_haskell.bat
+++ start_haskell.bat
@@ -1,3 +1,4 @@
 @echo off
-set PATH=c:\minghc-7.10.2-x86_64\bin;c:\minghc-7.10.2-x86_64\ghc-7.10.2\bin;c:\minghc-7.10.2-x86_64\git-2.4.5.1\bin;c:\cygwin\bin
+set PATH=c:\minghc-7.10.2-x86_64\bin;c:\minghc-7.10.2-x86_64\ghc-7.10.2\bin;c:\minghc-7.10.2-x86_64\git-2.4.5.1\bin;c:\cygwin\bin;c:\Python27;c:\WINDOWS\system32
+REM set PATH=c:\minghc-7.10.2-x86_64\bin;c:\minghc-7.10.2-x86_64\ghc-7.10.2\bin;c:\minghc-7.10.2-x86_64\git-2.4.5.1\bin;c:\cygwin\bin
 cd C:\Users\ao1\code\hk
