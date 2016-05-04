9a180c1286cffbbf7bda2b74ba10e1b332969e13
ao1 <ao1@ra.rockwell.com>
2016-05-04 11:54:34 +0800

    use better names

--- covMergeAnalysis.hs
+++ covMergeAnalysis.hs
@@ -3,33 +3,33 @@ import Data.List.Split (splitOn)
 import Data.List (groupBy, sortBy)
 data Funcinfolist = Funcinfolist { funcName :: String
                                  , lineCount :: Int
-                                 , coverageInfoList ::[(Int,Int)]} deriving (Show,Read)
-compareInfoList :: Int -> [Int] -> [Int] -> Bool
-compareInfoList diff a b = all (\x-> fst x - diff == snd x) $zip a b
+                                 , coverageInfoList ::[(Int,Int)]
+                                 , xmlFile :: String} deriving (Show,Read)
+compareFuncBody :: Int -> [Int] -> [Int] -> Bool
+compareFuncBody diff a b = all (\x-> fst x - diff == snd x) $zip a b
 
 mergeCoverageInfoList :: [(Int,Int)] -> [(Int,Int)] -> [(Int,Int)]
 -- 0 represents a covered line; x!!1 * y!!1 merges coverage result from 2 test data sources.
 -- partially covered line will show up as a 4 digit number
 mergeCoverageInfoList a b = zipWith (\x y-> (fst x, if snd x == 1 then 1000 * snd y
                                                     else if snd y == 1 then 1000 * snd x else snd y * snd x )) a b
-f :: Funcinfolist -> Funcinfolist -> Funcinfolist
-f a b = Funcinfolist (funcName a) (lineCount a) (mergeCoverageInfoList (coverageInfoList a) (coverageInfoList b))
+merge :: Funcinfolist -> Funcinfolist -> Funcinfolist
+merge a b = Funcinfolist (funcName a) (lineCount a) (mergeCoverageInfoList (coverageInfoList a) (coverageInfoList b)) (xmlFile a)
 
-_compareInfoList :: Funcinfolist -> Funcinfolist -> Bool
-_compareInfoList a b 
+compareInfoList :: Funcinfolist -> Funcinfolist -> Bool
+compareInfoList a b 
     | funcName a /= funcName b = False
     | lineCount a /= lineCount b = False
-    | otherwise = compareInfoList (listA !!0 - listB!!0 ) listA listB
+    | otherwise = compareFuncBody (listA !!0 - listB!!0 ) listA listB
     where  listA = map fst $coverageInfoList a
            listB = map fst $coverageInfoList b
 
 
-analyse :: Funcinfolist -> (String,Bool)
-analyse a = (funcName a,all (\x -> snd x == 0 ) (coverageInfoList a))
+allLinesCovered :: Funcinfolist -> (Funcinfolist,Bool)
+allLinesCovered a = (a,all (\x -> snd x == 0 ) (coverageInfoList a))
 
 main = do
   c <- getContents
   let funcInfoLists = ((map $splitOn "$") . lines ) c
---  let result = scanl1 f (map (\x -> Funcinfolist  (x!!0) (read (x!!1)) (read (x!!5))) funcInfoLists)
-  let result = map analyse $map (foldl1 f) $groupBy _compareInfoList  (map (\x -> Funcinfolist  (x!!0) (read (x!!1)) (read (x!!5))) funcInfoLists)
+  let result = map allLinesCovered $map (foldl1 merge) $groupBy compareInfoList  (map (\x -> Funcinfolist  (x!!0) (read (x!!1)) (read (x!!5)) (x!!6) ) funcInfoLists)
   mapM print  $filter (\x-> snd x == False ) result
