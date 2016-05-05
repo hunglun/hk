e43f7255ad14327dab882c8d68c4820a8d25a5ca
ao1 <ao1@ra.rockwell.com>
2016-05-05 17:22:14 +0800

    first working version
    this program takes a symbolname as input and output a tree of callers in
    depth first traversal.

--- cscopeTree.hs
+++ cscopeTree.hs
@@ -7,30 +7,39 @@ import Data.List (nub,sort)
 type SymbolName = String
 data ScopeInfo = ScopeInfo {symbolName :: SymbolName
                            ,filePath :: FilePath
+                            ,index :: String
                            } deriving (Read,Show)
 
-f :: SymbolName -> IO (ScopeInfo, [SymbolName])
-f s = do
-  c <- readCreateProcess (shell ("cscope -dL0 " ++ s)) ""
-  let results = lines c
-  let fpath = ((!! 0) . splitOn " " . (!! 0) ) results
-  let callerSymbolList = nub $filter ((`notElem` [s,"<global>"])) $map ((!! 1) . splitOn " ") $drop 1 results
-  print s
-  print callerSymbolList
-  print "--"
-  return (ScopeInfo s fpath ,callerSymbolList)
+removeDup :: (Eq a, Eq b) => [(a,b)] -> [(a,b)]
+removeDup [] = []
+removeDup [x] = [x]
+removeDup (x:y:xs) = if fst x == fst y then (removeDup (x:xs)) else x:(removeDup (y:xs))
 
-ioUnfoldTree :: (SymbolName -> IO (ScopeInfo, [SymbolName])) -> SymbolName -> IO (Tree ScopeInfo)
+f :: ScopeInfo -> IO (ScopeInfo, [ScopeInfo])
+f s = do
+  c <- readCreateProcess (shell ("cscope -dL0 " ++ (symbolName s))) ""
+  let results = lines c
+  let fpath = ((!! 0) . (!! 0) . filter  (\x -> x!!1 == symbolName s) . map (splitOn " ")  ) results
+  let callers = filter (\x -> x!!1 `notElem` [symbolName s,"<global>","defined"]) $map ( splitOn " ") results
+  let callersUniq = removeDup $map (\x-> (x!!1,x!!0)) callers
+  print [symbolName s,index s]
+  print (map fst callersUniq)
+  print "-------------------------------------------------------------"
+  if (length $index s) < 3 then 
+      return (ScopeInfo (symbolName s) fpath (index s) , map (\((x,y),z) -> ScopeInfo  x y ((index s)++(show z)) ) (zip callersUniq [1..] ))             
+  else
+      return (ScopeInfo (symbolName s) fpath (index s) , [])            
+
+ioUnfoldTree :: (ScopeInfo -> IO (ScopeInfo, [ScopeInfo])) -> ScopeInfo -> IO (Tree ScopeInfo)
 ioUnfoldTree f s = do
   (x, xs) <- f s
   forest <- ioUnfoldForest f xs
   return (Node x forest)
-ioUnfoldForest :: (SymbolName -> IO (ScopeInfo, [SymbolName])) -> [SymbolName] -> IO [Tree ScopeInfo]
-ioUnfoldForest f [] = return []
+  
+ioUnfoldForest :: (ScopeInfo -> IO (ScopeInfo, [ScopeInfo])) -> [ScopeInfo] -> IO [Tree ScopeInfo]
 ioUnfoldForest f bs = sequence $map (ioUnfoldTree f) bs
 
 main = do
-  print "hello"
   args <- getArgs
-  result <- ioUnfoldTree f (args!!0)  
-  print  $(levels result)!!1
+  result <- ioUnfoldTree f (ScopeInfo (args!!0) "" "0")
+  print ""
