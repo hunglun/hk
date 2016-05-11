48e4b062473d4aaade73d5ce334a84b7e3bf93d3
ao1 <ao1@ra.rockwell.com>
2016-05-11 09:49:43 +0800

    rename helper functions; support python test script

--- covMergeAnalysis.hs
+++ covMergeAnalysis.hs
@@ -1,5 +1,5 @@
 -- TODO : print out partially covered line and its xml file for furthur checking.
-import System.FilePath (takeDirectory)
+import System.FilePath (takeDirectory,splitPath)
 import Data.List.Split (splitOn)
 import Data.List (groupBy, sortBy, isInfixOf)
 data Funcinfolist = Funcinfolist { funcName :: String
@@ -8,6 +8,9 @@ data Funcinfolist = Funcinfolist { funcName :: String
                                  , xmlFile :: FilePath
                                  , sourceCode ::[(Int,String)]                                   
                                  } deriving (Show,Read)
+
+data Outcome = CompleteCoverage | IncompleteCoverage | UnusedFunction | HardwareException | NoTestRequired deriving (Show,Eq,Ord)
+
 compareFuncBody :: Int -> [Int] -> [Int] -> Bool
 compareFuncBody diff a b = all (\x-> fst x - diff == snd x) $zip a b
 
@@ -27,7 +30,6 @@ compareInfoList a b
     where  listA = map fst $coverageInfoList a
            listB = map fst $coverageInfoList b
 
-data Outcome = CompleteCoverage | IncompleteCoverage | UnusedFunction | HardwareException | NoTestRequired deriving (Show,Eq,Ord)
 eval :: Funcinfolist -> Outcome
 eval a
     | allLinesCovered a == True = CompleteCoverage
@@ -40,7 +42,7 @@ allLinesCovered :: Funcinfolist -> Bool
 allLinesCovered a = all (\x -> snd x == 0 ) (coverageInfoList a)
 
 allLinesUncovered :: Funcinfolist -> Bool
-allLinesUncovered a = all (\x -> snd x == 2 ) (coverageInfoList a)
+allLinesUncovered a = all (\x -> snd x >= 2 && snd x < 1000 ) (coverageInfoList a)
 
 readSrcLine :: FilePath -> Int -> IO String
 readSrcLine path lineNum = do
@@ -53,28 +55,29 @@ hardwareException a =  "HW_HandleException" `isInfixOf` a
 allHardwareException :: [(Int,Int)]-> [(Int,String)]  -> Bool
 allHardwareException b a = all (==True) $map (\(lineNum,s)-> ((covResult lineNum > 1000||covResult lineNum==1) && (hardwareException s)) || covResult lineNum == 0 ) a
                            where covResult lineNum=snd $head $filter (\x-> fst x==lineNum) b
-f :: Funcinfolist -> IO [String]
-f a = sequence $map (\(l,r) -> readSrcLine path l) (coverageInfoList a)
-      where path = (takeDirectory $xmlFile a) ++ "/_cases.cpp" -- if it is aut file
+generateSourceCode :: Funcinfolist -> IO [String]
+generateSourceCode a = sequence $map (\(l,r) -> readSrcLine path l) (coverageInfoList a)
+      where autpath = (takeDirectory $xmlFile a) ++ "/_cases.cpp" -- if it is aut file
+            pypath = (takeDirectory $xmlFile a) ++ "/unitTest.c" -- if it is python file
+            path = if "_aut\\" `isInfixOf` ((!!0) $splitPath $xmlFile a) then autpath else pypath
+updateFuncInfoList :: [[String]] -> [Funcinfolist] -> [Funcinfolist]
+updateFuncInfoList a b = zipWith (\x y -> updateFuncInfoListHelper x y) a b 
 
-z :: [[String]] -> [Funcinfolist] -> [Funcinfolist]
-z a b = zipWith (\x y -> k x y) a b 
+updateFuncInfoListHelper :: [String] -> Funcinfolist -> Funcinfolist
+updateFuncInfoListHelper a b = Funcinfolist (funcName b) (lineCount b) (coverageInfoList b) (xmlFile b) sc
+      where sc = makeLineSourcePair a (coverageInfoList b)
 
-g :: [String] -> [(Int,Int)] -> [(Int,String)]
-g a b = zipWith (\x y->(fst y, x )) a b
+makeLineSourcePair :: [String] -> [(Int,Int)] -> [(Int,String)]
+makeLineSourcePair a b = zipWith (\x y->(fst y, x )) a b
 
-k :: [String] -> Funcinfolist -> Funcinfolist
-k a b = Funcinfolist (funcName b) (lineCount b) (coverageInfoList b) (xmlFile b) sc
-      where sc = g a (coverageInfoList b)
         
 main = do
-  c <- getContents
-  
-  let funcInfoLists = map (\x -> x ++ [[]]) $((map $splitOn "$") . lines ) c
-  let funcInfoLists_ = map (\x -> Funcinfolist  (x!!0) (read (x!!1)) (read (x!!5)) (x!!6) (read (x!!7))) funcInfoLists
-  temp <- mapM f funcInfoLists_
-  let funcInfoLists__ = z temp funcInfoLists_
-  let result = map (\x-> (x,eval x)) $map (foldl1 merge) $groupBy compareInfoList  funcInfoLists__
---  mapM print $sortBy (\x y-> if (snd x) > (snd y) then GT else LT ) $map (\(x,y)->((funcName x,(coverageInfoList x, xmlFile x)),y)) result
-  mapM print $sortBy (\x y-> if (snd x) > (snd y) then GT else LT ) $map (\(x,y)->(take 50 $funcName x,y)) result
+  c <- getContents  
+  let funcInfoListsData = map (\x -> x ++ [[]]) $((map $splitOn "$") . lines ) c
+  let funcInfoLists = map (\x -> Funcinfolist  (x!!0) (read (x!!1)) (read (x!!5)) (x!!6) (read (x!!7))) funcInfoListsData
+  sourceCodeList <- mapM generateSourceCode funcInfoLists
+  let updatedFuncInfoLists = updateFuncInfoList sourceCodeList funcInfoLists
+  let result = map (\x-> (x,eval x)) $map (foldl1 merge) $groupBy compareInfoList  updatedFuncInfoLists
+  mapM print $sortBy (\x y-> if (fst x) > (fst y) then GT else LT ) $map (\(x,y)->(y,(funcName x,(coverageInfoList x, xmlFile x)))) result
+--  mapM print $sortBy (\x y-> if (snd x) > (snd y) then GT else LT ) $map (\(x,y)->(take 50 $funcName x,y)) result
 
